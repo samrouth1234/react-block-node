5a46aca5eb04a01cfdacf26995ffd484ed83c528
Samrouth <bichsamroth@gmail.com>
2024-07-10 16:36:03 +0700

    logout data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "@blocknote/core/fonts/inter.css";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
@@ -5,11 +6,26 @@ import "@blocknote/mantine/style.css";
 import './App.css';
 
 function App() {
+  const [blocks, setBlocks] = useState([]); // useState<NoteBlock[]
   const editor = useCreateBlockNote();
+  console.log(blocks);
   return (
-    <div >
+    <div>
       <h1>Hello BlockNote!</h1>
-        <BlockNoteView editor={editor} theme={"light"} className="main" />
+      <BlockNoteView 
+          editor={editor}
+          theme={"light"} 
+          className="main"
+          onChange={() => {
+            // Saves the document JSON to state.
+            setBlocks(editor.document);
+          }}
+          />
+        <div className={"item bordered"}>
+        <pre>
+          <code>{JSON.stringify(blocks, null, 2)}</code>
+        </pre>
+      </div>
     </div>
   );
 }
